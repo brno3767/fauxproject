e45c56bbf5be58695bd61df83fb2ad124660242b
Brady Noble <brno3767@colorado.edu>
2016-10-25 22:40:42 -0600

    adding home page and home/css

--- config.rb
+++ config.rb
@@ -1,10 +1,11 @@
 require 'compass/import-once/activate'
+require 'susy'
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "css"
-sass_dir = "sass"
+sass_dir = "scss"
 images_dir = "img"
 javascripts_dir = "js"
 
@@ -13,7 +14,7 @@ javascripts_dir = "js"
 output_style = :compact
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 line_comments = false
