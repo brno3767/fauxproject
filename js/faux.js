5d6259703ec7f15d6c176d27a23144662a0b56ac
Brady Noble <brno3767@colorado.edu>
2016-10-26 22:03:46 -0600

    added javascript and contact page

---
+++ js/faux.js
@@ -0,0 +1,10 @@
+function quote(){
+
+	var end;
+
+	end=alert("We have received your information! You will hear from us soon...");
+}
+
+
+var result=document.getElementById("submit");
+result.addEventListener("submit", quote, false);
